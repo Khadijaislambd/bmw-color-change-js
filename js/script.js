2bb15c7dd55a3b0a4acb9520cd8c79e3f07f137c
Khadijaislambd <contact.khadijabegum@gmail.com>
2021-03-31 19:53:22 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+var myVar = document.querySelector("#myImageId");
+function picture1(){
+    myVar.src = "img/red.png";
+}
+function picture2(){
+    myVar.src = "img/green.png";
+}
+function picture3(){
+    myVar.src = "img/dred.png";
+}
+function picture4(){
+    myVar.src = "img/sky.png";
+}
